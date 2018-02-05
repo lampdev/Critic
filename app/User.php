5076085a1f1af2d1898a6a1ca17c0831af92a916
gderiyenko <gleb.deriyenko@gmail.com>
2018-02-05 12:49:03 +0200

    Revert "added auth"
    This reverts commit a521969eb76758106c09da28a8a9f985be43d230.

--- app/User.php
+++ app/User.php
@@ -24,6 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'role_id'
+        'password', 'remember_token',
     ];
 }
--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+define('LARAVEL_START', microtime(true));
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader

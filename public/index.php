a521969eb76758106c09da28a8a9f985be43d230
gderiyenko <gleb.deriyenko@gmail.com>
2018-02-05 12:28:51 +0200

    added auth

--- public/index.php
+++ public/index.php
@@ -6,8 +6,6 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-define('LARAVEL_START', microtime(true));
-
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader

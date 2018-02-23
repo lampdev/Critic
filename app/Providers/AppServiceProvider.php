74900f7a7f07e39a268143235a87c4abf8daa426
alexey <derienko@qarea.com>
2018-02-23 14:15:41 +0100

    * adding docker-compose.yml * adding docker config/log files * fixing https://laravel-news.com/laravel-5-4-key-too-long-error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Providers;
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -10,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
     /**
      * Register any application services.
